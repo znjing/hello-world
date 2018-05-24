fd7e850530f2c7502670d9ec4fc21b99f4c1bc2d
znjing <39575410+znjing@users.noreply.github.com>
2018-05-24 11:20:41 +0800

    Create string.cpp
    string

---
+++ string.cpp
@@ -0,0 +1,25 @@
+
++
+{
+ int len=a.len+b.len;
+ char*rep1=new char[len+1];
+ strcpy(rep1,a.rep);
+ strcat(rep1,b.rep);
+ String str(rep1);
+ delete rep1;
+ return str;
+ 
+ 
+}
+
++=
+{
+  this->len+=a.len;
+  char*rep1=this->rep;
+  rep=new char[len+_1];
+  strcpy(rep,rep1);
+  strcat(rep,a.rep);
+  delete rep1;
+  return *this;
+ 
+}
